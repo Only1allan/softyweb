2dccce23e50fcd423076818e5a17c3cc5da402c6
Allan Kariuki <kariukiallan850@gmail.com>
2024-07-30 23:16:22 +0300

    first commit

---
+++ CMakeFiles/softyweb.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Channel.cpp" "softyweb/CMakeFiles/softyweb.dir/Channel.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Channel.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Epoll.cpp" "softyweb/CMakeFiles/softyweb.dir/Epoll.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Epoll.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/EventLoop.cpp" "softyweb/CMakeFiles/softyweb.dir/EventLoop.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/EventLoop.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/EventLoopThread.cpp" "softyweb/CMakeFiles/softyweb.dir/EventLoopThread.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/EventLoopThread.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/EventLoopThreadPool.cpp" "softyweb/CMakeFiles/softyweb.dir/EventLoopThreadPool.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/EventLoopThreadPool.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/HttpData.cpp" "softyweb/CMakeFiles/softyweb.dir/HttpData.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/HttpData.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Main.cpp" "softyweb/CMakeFiles/softyweb.dir/Main.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Main.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Server.cpp" "softyweb/CMakeFiles/softyweb.dir/Server.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Server.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Timer.cpp" "softyweb/CMakeFiles/softyweb.dir/Timer.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Timer.cpp.o.d"
+  "/home/dev254/Desktop/amis/WebServer/softyweb/Util.cpp" "softyweb/CMakeFiles/softyweb.dir/Util.cpp.o" "gcc" "softyweb/CMakeFiles/softyweb.dir/Util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dev254/Desktop/amis/WebServer/softyweb/base/CMakeFiles/libserver_base.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/softyweb.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/softyweb.dir/Channel.cpp.o"
+  "CMakeFiles/softyweb.dir/Channel.cpp.o.d"
+  "CMakeFiles/softyweb.dir/Epoll.cpp.o"
+  "CMakeFiles/softyweb.dir/Epoll.cpp.o.d"
+  "CMakeFiles/softyweb.dir/EventLoop.cpp.o"
+  "CMakeFiles/softyweb.dir/EventLoop.cpp.o.d"
+  "CMakeFiles/softyweb.dir/EventLoopThread.cpp.o"
+  "CMakeFiles/softyweb.dir/EventLoopThread.cpp.o.d"
+  "CMakeFiles/softyweb.dir/EventLoopThreadPool.cpp.o"
+  "CMakeFiles/softyweb.dir/EventLoopThreadPool.cpp.o.d"
+  "CMakeFiles/softyweb.dir/HttpData.cpp.o"
+  "CMakeFiles/softyweb.dir/HttpData.cpp.o.d"
+  "CMakeFiles/softyweb.dir/Main.cpp.o"
+  "CMakeFiles/softyweb.dir/Main.cpp.o.d"
+  "CMakeFiles/softyweb.dir/Server.cpp.o"
+  "CMakeFiles/softyweb.dir/Server.cpp.o.d"
+  "CMakeFiles/softyweb.dir/Timer.cpp.o"
+  "CMakeFiles/softyweb.dir/Timer.cpp.o.d"
+  "CMakeFiles/softyweb.dir/Util.cpp.o"
+  "CMakeFiles/softyweb.dir/Util.cpp.o.d"
+  "softyweb"
+  "softyweb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/softyweb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
